49e0e24f7bd16e86b525b7e8b31f06104f035fc9
nurlasova <nurlasova.kg@gmail.com>
2021-04-19 10:40:55 +0600

    I added new files

---
+++ src/js/shop.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper1', {
+    // Optional parameters
+    slidesPerView:3,
+    slidesPerGroup:1,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    slidesPerView:3,
+    slidesPerGroup:1,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
